05a6d7e8a8d37f29facd7bcd4f624beafb1ac26d
itsPoipoi <61839795+itsPoipoi@users.noreply.github.com>
2025-12-07 17:15:23 +0100

    Update PowerShell profile download path

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,2 @@
-$env:DOCUMENTS = [Environment]::GetFolderPath("MyDocuments")
-curl -sL https://raw.githubusercontent.com/itsPoipoi/powershell/refs/heads/main/Microsoft.PowerShell_profile.ps1 -o "$env:DOCUMENTS\PowerShell\Microsoft.PowerShell_profile.ps1" --create-dirs
+curl -sL https://raw.githubusercontent.com/itsPoipoi/powershell/refs/heads/main/Microsoft.PowerShell_profile.ps1 -o $profile --create-dirs
 & $profile
